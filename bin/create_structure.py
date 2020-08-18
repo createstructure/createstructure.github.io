41c4df455a452d118d741b77cf4b67b8ca31d626
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-08-18 11:10:34 +0200

    Created bin/template.py

---
+++ bin/create_structure.py
@@ -0,0 +1,12 @@
+"""create_structure
+"""
+
+__author__ = "davidecastellani@castellanidavide.it"
+__version__ = "01.01 2020-8-18"
+
+class create_structure:
+	def __init__ (self):
+		print("create_structure")
+
+if __name__ == "__main__":
+	create_structure()
