bceb6ddac6c9bee08247dbf7887017134388e33c
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-12 13:26:59 +0000

    another test :(

--- bin/settings.py
+++ bin/settings.py
@@ -1,4 +1,4 @@
 TOKEN = "TODO"
 SOUCES_OF_TEMPLATES = ["TODO", "CastellaniDavide"]
 ORGANIZATION_NAME = "TODO" # Leave empty if you want to add into your personal GitHub account
-IGNORE_FOLDERS = ["TODO", ".vscode", ".vs"]
+IGNORE_FOLDERS = ["TODO", ".vs"]
