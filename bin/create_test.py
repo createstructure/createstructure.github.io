6e247e73b3e50d5fb221d522d476838eac176c6d
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-13 20:31:17 +0000

    Make file by python code

---
+++ bin/create_test.py
@@ -0,0 +1,6 @@
+"""Only a programm to make example for executions
+"""
+
+import time
+
+open("testfile.txt", "w+").write(f"{int(round(time.time() * 1000))}\ndefault\nhttps://github.com/CastellaniDavide/create_structure/blob/master/.github/workflows/test.yml\ntest\nn\nn\n")
