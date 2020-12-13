e6cf4717d99b2683eb9863a3a4afcad90840594a
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-13 20:34:07 +0000

    Added a log string

--- bin/create_test.py
+++ bin/create_test.py
@@ -4,3 +4,4 @@
 import time
 
 open("testfile.txt", "w+").write(f"{int(round(time.time() * 1000))}\ndefault\nhttps://github.com/CastellaniDavide/create_structure/blob/master/.github/workflows/test.yml\ntest\nn\nn\n")
+print("File created")
