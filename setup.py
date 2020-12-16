d5aafc8f4b6c3fb20b04e4bf2d6cde78c53d627c
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-16 07:26:05 +0000

    First test of pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='create_structure',
+      version='5.6',
+      description='This is the magic bot by Castellani Davide. With this programm you can easly create a repository on GitHub with a basic template, personalized for your use.',
+      url='https://github.com/CastellaniDavide/create_structure',
+      author='CastellaniDavide',
+      author_email='help@castellanidavide.it',
+      license='GNU',
+      packages=find_packages(),
+      python_requires=">=3.7",
+      platforms="linux_distibution",
+      install_requires=[
+        "DateTime>=4.3",
+        "PyGithub>=1.54",
+        "requests>=2.24.0"
+			],
+      zip_safe=True
+      )
