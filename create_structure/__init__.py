122df66afb58fd76afb99f76a4e9d9bfd59cb02a
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-03 16:51:59 +0000

    Fixed a bug

--- create_structure/__init__.py
+++ create_structure/__init__.py
@@ -208,8 +208,11 @@ class create_structure:
 		contents = self.template.get_contents(f"{loc}")
 		for content_file in sorted(contents, reverse=True, key=create_structure.name_of_path): # Put .folders at the end
 			if not content_file.path in [".castellanidavide", ""] + self.IGNORE:
-				if content_file.path == ".github/workflows": # Wait the end of others before do workflows
-					while (active_count() != 2): print(active_count())
+				try:
+					if content_file.path == ".github/workflows": # Wait the end of others before do workflows
+						while (active_count() != 2): pass
+				except:
+					pass
 
 				if content_file.type == "file":
 					Thread(target = self.create_file, args = (self.change(content_file.path), f"{self.change(wget(f'https://raw.githubusercontent.com/{self.template_name}/master/{content_file.path}').text)}")).start()
