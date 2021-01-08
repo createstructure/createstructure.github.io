fd6a2cbd41c73aa3548e98d92e0ca12a84e0bb0b
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-08 22:34:44 +0000

    It runs in a correct way

--- debian/createstructure.c
+++ debian/createstructure.c
@@ -4,84 +4,59 @@
 #include <string.h>
 
 // Support functions
-char* path();
-char* connect_strings(char* first, char* second);
 int exist (int argc, char **argv, char *item);
 int min(int a, int b);
+char* connect_strings(char* first, char* second);
+char* read_by_terminal(char* input);
+char* choppy(char *s);
+void check_pip_package_version();
+void get_setup();
+
+// Action functions
+void help();
+void set_verbose();
+void version();
+void login();
+void createstructure();
 
 // Global variables
 bool verbose = false;
+char* TOKEN;
+char* SOURCES;
+char* ORGANIZATION_NAME;
+char* IGNORE;
 
 int main(int argc, char **argv)
 {
-	if (argc == 1 || exist(argc, argv, "-h") || exist(argc, argv, "--help"))
+	if (exist(argc, argv, "-h") || exist(argc, argv, "--help"))
 	{
-		printf("---Help---\n");
-	}
-		
+		help(argc, argv);
+		return 0;
+	}	
 	if (exist(argc, argv, "-v") || exist(argc, argv, "--verbose"))
 	{
-		printf("---Verbose---\n");
-		verbose = true;
-		printf("Enabled verbose\n");
+		set_verbose();
+		//return 0;
 	}
 		
 	if (exist(argc, argv, "-V") || exist(argc, argv, "--version"))
 	{
-		printf("---Version---\n");
-		if (verbose)
-			system("pip3 show createstructure");
-		else
-			system("pip3 freeze | grep createstructure");		
+		version();
+		return 0;
 	}
 	
 	if (exist(argc, argv, "-l") || exist(argc, argv, "--login"))
-		printf("Login\n");
-		
-	if (exist(argc, argv, "-t") || exist(argc, argv, "--token"))
-		printf("Token\n");
-		
-	for(int i = 0; i < argc; ++i)
-		printf("%d. %s\n", i, argv[i]);
-	
-	// Get wanted path
-	char* p = path();
-	
-	// Delate last results if exists
-	system(connect_strings("sed -i '/alias createstructure/d' ", p));
+	{
+		login();
+		return 0;
+	}
 
-	// Create file puntator
-	FILE *pAlias;
-	pAlias = fopen(p, "a+");
-	
-	// Print on my file
-	fprintf(pAlias, "alias createstructure='echo hiii'\n");
-	
+	// check_pip_package_version(); // Not necessary
+	get_setup();
+	createstructure();	// Run core
+
 	// End
-	printf("Done\n");
-	fclose(pAlias);
 	return 0;
-}
-
-char* path()
-{
-	// Get home path
-	char* home = getenv("HOME");
-
-	// Save additional part of the path
-	char* path = "/.bash_aliases";
-	
-	// Return the final path variable
-	return connect_strings(home, path);
-}
-
-char* connect_strings(char* first, char* second)
-{
-	char* final = malloc(strlen(first) + strlen(second) + 1);
-	strcpy(final, first);
-	strcat(final, second);
-	
-	return final;
 }
 
 int exist (int argc, char **argv, char *item)
@@ -98,4 +73,131 @@ int min(int a, int b)
 	if (a > b)
 		return a;
 	return b;
+}
+
+char* connect_strings(char* first, char* second)
+{
+	char* final = malloc(strlen(first) + strlen(second) + 1);
+	strcpy(final, first);
+	strcat(final, second);
+	
+	return final;
+}
+
+char* read_by_terminal(char* input)
+{
+	FILE *cmd = popen(input, "r");
+    char *result = malloc(1024); // max return size
+    char *result2 = malloc(1024); // max return size
+	result2 = "";
+    while (fgets(result, sizeof(result), cmd) !=NULL)
+        result2 = connect_strings(result2, result);
+	pclose(cmd);
+	return choppy(result2);
+}
+
+char* choppy(char *s)
+{
+    char *n = malloc( strlen( s ? s : "\n" ) );
+    if( s )
+        strcpy( n, s );
+    n[strlen(n)-1]='\0';
+    return n;
+}
+
+void check_pip_package_version()
+{
+	if(read_by_terminal("dpkg -s createstructure | grep -i version | sed 's/Version: //'") != read_by_terminal("pip3 freeze | grep createstructure | sed 's/createstructure==//'"))
+	{
+		char* install_string = connect_strings("pip3 install createstructure==", read_by_terminal("dpkg -s createstructure | grep -i version | sed 's/Version: //'"));
+
+		if (verbose)
+			system(install_string);
+		else
+			read_by_terminal(install_string);
+	}
+}
+
+void get_setup()
+{
+	TOKEN = read_by_terminal("cat /etc/createstructure.conf | grep 'token' | sed 's/token=//' | sed 's/token= //'");
+	SOURCES = read_by_terminal("cat /etc/createstructure.conf | grep 'sources' | sed 's/sources=//' | sed 's/sources= //'");
+	ORGANIZATION_NAME = read_by_terminal("cat /etc/createstructure.conf | grep 'organization_name' | sed 's/organization_name=//' | sed 's/organization_name= //'");
+	IGNORE = read_by_terminal("cat /etc/createstructure.conf | grep 'ignore' | sed 's/ignore=//' | sed 's/ignore= //'");
+}
+
+void help(int argc, char **argv)
+{
+	if (exist(argc, argv, "-v") || exist(argc, argv, "--verbose"))
+	{
+		printf("---Help---\n");
+		printf("Open man...\n");
+	}
+	system("man createstructure");
+}
+
+void set_verbose()
+{
+	printf("---Verbose---\n");
+	verbose = true;
+	printf("Enabled verbose\n");
+}
+
+void version()
+{
+	if (verbose)
+		printf("---Version---\n");
+
+	if (verbose)
+		system("apt-cache policy createstructure");
+	else
+		system("dpkg -s createstructure | grep -i version | sed 's/Version: //'");		
+}
+
+void login()
+{
+	char TEMP_TOKEN[50] = {'\0'};
+	char TEMP_SOURCES[200] = {'\0'};
+	char TEMP_ORGANIZATION_NAME[100] = {'\0'};
+	char TEMP_IGNORE[100] = {'\0'};
+
+	if (verbose)
+		printf("---Login---\n");
+	printf("\u2753 Insert your token: ");
+	scanf("%50s", TEMP_TOKEN);
+
+	printf("\u2753 Insert your sources (format [item1,item2,...]): ");
+	scanf("%200s", TEMP_SOURCES);
+
+	printf("\u2753 Insert your organization name or 'p' if you want to use your personal account: ");
+	scanf("%100s", TEMP_ORGANIZATION_NAME);
+	if (TEMP_ORGANIZATION_NAME[0] == 'p')
+		TEMP_ORGANIZATION_NAME[0] = '\0';
+
+	printf("\u2753 Insert the folder to ignore (format [item1,item2,...]): ");
+	scanf("%100s", TEMP_IGNORE);
+
+	if (verbose)
+		printf("\n---Infos---\n\u2139 TOCKEN: %s\n\u2139 SOURCES: %s\n\u2139 ORGANIZATION_NAME (empty if you will use your personal account): %s\n\u2139 IGNORE: %s\n", &TEMP_TOKEN, &TEMP_SOURCES, &TEMP_ORGANIZATION_NAME, &TEMP_IGNORE);
+
+	// Save insert options
+	char execution_string[2048] = {'\0'};
+	sprintf(execution_string, "sudo echo 'token=%s\nsources=%s\norganization_name=%s\nignore=%s' > /etc/createstructure.conf", TEMP_TOKEN, TEMP_SOURCES, TEMP_ORGANIZATION_NAME, TEMP_IGNORE);
+	system(execution_string);
+
+	if (verbose)
+		printf("%s", "Configuration saved");
+}
+
+void createstructure()
+{
+	if (verbose)
+		printf("---Run-code---\n");
+
+	// Compose my string
+	char execution_string[2048] = {'\0'};
+	sprintf (execution_string, "python3 -c \"exec(\\\"from create_structure import create_structure;create_structure()\\\")\" -t=%s -s=%s -o=%s -i=%s %s", TOKEN, SOURCES, ORGANIZATION_NAME, IGNORE, (verbose ? "-v" : ""));
+
+	printf("%s\n", execution_string);
+	system(execution_string);
 }
