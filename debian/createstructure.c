55ef2638fd2fde3c8f6e6ce912e089d3fcafeee9
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-03-10 17:32:28 +0000

    python3 -m pip3 freeze

--- debian/createstructure.c
+++ debian/createstructure.c
@@ -424,7 +424,7 @@ void createstructure()
 	char execution_string[2048] = {'\0'};
 #ifdef UBUNTU
 	//sprintf (execution_string, "python3 -c \"exec(\\\"from createstructure import createstructure;createstructure()\\\")\" -t=%s -s=%s -o=%s -i=%s %s", TOKEN, SOURCES, ORGANIZATION_NAME, IGNORE, (verbose ? "-v" : ""));
-	sprintf (execution_string, "pip3 freeze | grep createstructure | sed 's/createstructure==//'; python3 -c \"exec(\\\"from createstructure import createstructure;createstructure()\\\")\" -t=%s -o=%s -i=%s %s", TOKEN, ORGANIZATION_NAME, IGNORE, (verbose ? "-v" : ""));
+	sprintf (execution_string, "python3 -m pip3 freeze | grep createstructure | sed 's/createstructure==//'; python3 -c \"exec(\\\"from createstructure import createstructure;createstructure()\\\")\" -t=%s -o=%s -i=%s %s", TOKEN, ORGANIZATION_NAME, IGNORE, (verbose ? "-v" : ""));
 #endif // UBUNTU
 #ifdef WINDOWS
 	//sprintf (execution_string, "python.exe -c \"exec(\\\"from createstructure import createstructure;createstructure()\\\")\" -t=%s -s=%s -o=%s -i=%s %s", TOKEN, SOURCES, ORGANIZATION_NAME, IGNORE, (verbose ? "-v" : ""));
